39fbdcd015241b83ca08816cec9eb2b675ccf517
Melody Monarch <oliva.sam.so@gmail.com>
2021-02-28 21:28:10 -0600

    dependencies and middleware as a basis added to start with

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const db = require("./models");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Middleware
+app.use(logger("dev")); // morgan
+
+app.use(express.urlencoded({extended:true})); //for post requests
+app.use(express.json()); // for post requests
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/skeletondb", {useNewUrlParser:true});
+
+
+
+// Listener 
+app.listen(PORT, () => {
+    console.log(`app running on http://localhost:${PORT}`);
+})
+
+
+
+
+
+
