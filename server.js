48a1d1d929ad0bf733c05c958d2e2161e42adee9
Melody Monarch <oliva.sam.so@gmail.com>
2021-03-02 14:24:06 -0600

    routes added to connect

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,6 +17,11 @@ app.use(express.json()); // for post requests
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {useNewUrlParser:true});
+
+const htmlRoutes = require("./routes/html-routes");
+const apiRoutes = require("./routes/api-routes")
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 // Listener 
 app.listen(PORT, () => {
